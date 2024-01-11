d67de3952322403a32cb55ef290ba1f183b9fb05
dmitryPatseyeu <dmitrypatseyeu@gmail.com>
2024-01-11 12:15:51 +0100

    refactor 1: add unsubscribe in user list component(implement async pipe)

--- src/app/core/user-list/user-list.component.ts
+++ src/app/core/user-list/user-list.component.ts
@@ -3,33 +3,26 @@ import {UserApiService} from "../../services/user/user-api.service";
 import {IUser} from "../../models/user.interface";
 import {MatGridListModule} from "@angular/material/grid-list";
 import {UserCardComponent} from "../user-card/user-card.component";
-import {NgForOf} from "@angular/common";
+import {AsyncPipe, NgForOf} from "@angular/common";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'user-list',
   templateUrl: 'user-list.component.html',
   styleUrls: ['./user-list.component.less'],
   standalone: true,
-  imports: [MatGridListModule, UserCardComponent, NgForOf]
+  imports: [MatGridListModule, UserCardComponent, NgForOf, AsyncPipe]
 })
 
 export class UserListComponent {
   title = 'user-list';
 
-  users: IUser[] = [];
+  users$:Observable<IUser[]> =  this.service.getUsers();
 
   constructor(private service: UserApiService) {
   }
 
   deleteCard(id: number | undefined) {
     console.log(id);
-  }
-
-  ngOnInit() {
-    this.service.getUsers()
-      .subscribe(response => {
-        this.users = response;
-        console.log(this.users)
-      });
   }
 }
